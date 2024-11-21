ec76e3cb6e96fb07a645626579a225db9b63d234
Zero1074 <zero2023xd@gmail.com>
2024-11-20 20:28:32 -0600

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,15 +1,18 @@
-
 const handler = async (m) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_unbanchat
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje;
+  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`));
+  const tradutor = _translate.plugins.owner_unbanchat;
 
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply(tradutor.texto1);
+  
+  m.reply("> Chat desbaneado con éxito. 🔓");
+  conn.sendMessage(m.chat, { react: { text: '🔓', key: m.key } });
 };
+
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
 handler.command = /^unbanchat$/i;
 handler.rowner = true;
+
 export default handler;
