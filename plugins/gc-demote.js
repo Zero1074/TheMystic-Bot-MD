3af65954875a8fb333567335d496159a18308350
Zero1074 <zero2023xd@gmail.com>
2024-11-24 20:49:16 -0600

    Update gc-demote.js

--- plugins/gc-demote.js
+++ plugins/gc-demote.js
@@ -1,21 +1,14 @@
-
-
 const handler = async (m, {conn, usedPrefix, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_demote
-
   if (isNaN(text) && !text.match(/@/g)) {
-
+    // Si no hay número ni usuario mencionado, hacer nada.
   } else if (isNaN(text)) {
     var number = text.split`@`[1];
   } else if (!isNaN(text)) {
     var number = text;
   }
 
-  if (!text && !m.quoted) return conn.reply(m.chat, `${tradutor.texto1[0]} ${usedPrefix}quitaradmin @tag*\n*┠≽ ${usedPrefix}quitaradmin ${tradutor.texto1[1]}`, m);
-  if (number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, tradutor.texto2, m);
+  if (!text && !m.quoted) return;
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) return;
 
   try {
     if (text) {
@@ -26,11 +19,20 @@ const handler = async (m, {conn, usedPrefix, text}) => {
       var user = number + '@s.whatsapp.net';
     }
   } catch (e) {
+    console.log(e);
   } finally {
+    // Reaccionar con un check (✔) al mensaje del comando /demote
+    try {
+      await conn.sendMessage(m.chat, { react: { text: '✔', key: m.key } });
+    } catch (e) {
+      console.error('Error al agregar la reacción:', e);
+    }
+
+    // Quitar el poder del usuario
     conn.groupParticipantsUpdate(m.chat, [user], 'demote');
-    conn.reply(m.chat, tradutor.texto3, m);
   }
 };
+
 handler.help = ['*593xxx*', '*@usuario*', '*responder chat*'].map((v) => 'demote ' + v);
 handler.tags = ['group'];
 handler.command = /^(demote|quitarpoder|quitaradmin)$/i;
@@ -38,4 +40,5 @@ handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
 handler.fail = null;
+
 export default handler;
